76579225b5b9e8693f5cb372a16b0a09ab66290a
Kyle C <kyclutter@gmail.com>
2020-08-05 09:11:42 -0500

    updates

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,4 +1,9 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Avatar from "@material-ui/core/Avatar";
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import {Link} from 'react-router-dom';
 import PropTypes from "prop-types";
 
 const Login = () => {
@@ -24,32 +29,44 @@ const Login = () => {
 
   return (
     <div className="login">
-      <h1 className="register__header">Login</h1>
-      <p className="register-text">Sign In to Your Account</p>
-      <form onSubmit={(e) => onSubmit(e)} className="form">
-        <div className="form-group">
-          <input
-            type="email"
-            placeholder="Email"
-            name="email"
-            value={email}
-            onChange={(e) => onChange(e)}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <input
-            type="password"
-            placeholder="Password"
-            name="password"
-            minLength="6"
-            value={password}
-            onChange={(e) => onChange(e)}
-          />
-        </div>
+      <div className="login__container">
+          <Avatar className="login__avatar">
+              <LockOutlinedIcon/>
+          </Avatar>
+        <h1 className="login__header">Login</h1>
+        <p className="login__text">Sign In to Your Account</p>
+        <form onSubmit={(e) => onSubmit(e)} className="form">
+          <div className="form-group">
+            <TextField
+              className="form__field"
+              type="email"
+              placeholder="Email"
+              name="email"
+              value={email}
+              onChange={(e) => onChange(e)}
+              label="Email"
+              variant="outlined"
+            />
+          </div>
+          <div className="form-group">
+            <TextField
+                className='form__field'
+              type="password"
+              placeholder="Password"
+              name="password"
+              minLength="6"
+              value={password}
+              onChange={(e) => onChange(e)}
+              id="outlined-basic"
+              label="Password"
+              variant="outlined"
+            />
+          </div>
+          <p className="form__text">Dont Have an Account? <Link to='/register'>Register</Link></p>
 
-        <input type="submit" placeholder="submit" />
-      </form>
+          <Button id="form__button" variant="contained" color="primary" type="submit" placeholder="submit" >Sign In</Button>
+        </form>
+      </div>
     </div>
   );
 };
