d82ae5f1218bf98d54f9e31ea82602515659a182
Kyle C <kyclutter@gmail.com>
2020-09-23 10:05:43 -0500

    updates

--- routes/api/listing.js
+++ routes/api/listing.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer')
 const listingController = require('../../controllers/listingController')
 
 
@@ -7,6 +8,37 @@ const listingController = require('../../controllers/listingController')
 // @route       GET api/listing
 //@dec          Add a new Listing 
 //@acesss       Public
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads');
+    },
+    filename: (req, file, cb) => {
+        console.log(file);
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype == 'image/jpeg' || file.mimetype == 'image/png') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
+const upload = multer({ storage: storage, fileFilter: fileFilter });
+
+
+router.post('/upload', upload.single('image'), (req, res, next) => {
+    try {
+        return res.status(201).json({
+            message: 'File uploded successfully'
+        });
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+
 router.post('/', listingController.addListing )
 
 
